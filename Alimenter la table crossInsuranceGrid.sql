285a90c1241cfdfacdffac0a0aae8a541351a2dc
tumtum237 <57261145+tumtum237@users.noreply.github.com>
2026-04-30 16:23:13 +0200

    Refactor crossInsuranceGrid.sql for clarity and updates
    Updated the script to create insurance correspondences between different providers. Added comments for clarity and verification steps.

--- Alimenter la table crossInsuranceGrid.sql
+++ Alimenter la table crossInsuranceGrid.sql
@@ -1,18 +1,11 @@
---assurance avec correspondance
-select cig.*, insu1.Name, insu1.providerid,  insu2.ProviderId  from CrossInsuranceGrid cig
-inner join Insurance insu1 on insu1.InsuranceId = cig.InsuranceId 
-inner join Insurance insu2 on insu2.InsuranceId = cig.DestinationInsuranceId
-order by insu1.ProviderId , insu1.Name,  insu2.ProviderId 
+--Ce script permet de créer une correspondance entre des assurances provenant de différents opérateurs
 
---assurance sans correspondance
-select insu.* from Insurance insu
-left join CrossInsuranceGrid cig on cig.InsuranceId = insu.InsuranceId 
-where cig.CrossInsuranceGridId is null
+--Dans un contexte de réservation de véhicules, un client peut réserver un véhicule appartenant à un autre opérateur que le sien
+--L’objectif est donc d’identifier automatiquement l’équivalent d’une assurance chez un autre opérateur afin d’appliquer la bonne tarification
 
---Assurance Avec Rachat partiel de franchise (Jeune Conducteur)	22	=>Assurance Avec Rachat partiel de franchise (0.56)
---Assurance Avec Rachat partiel de franchise (Pro)	22 => Assurance Avec Rachat partiel de franchise (0.28)
---Malus Sinistre	18 => Malus Sinistre (0.28)
---Assurance Pro avec rachat partiel de franchise	19 => Assurance Avec Rachat partiel de franchise (0.28)
+--Si deux assurances ont le même nom mais appartiennent à des opérateurs différents alors elles sont considérées comme équivalentes
+
+--Ces correspondances sont enregistrées dans la table CrossInsuranceGrid 
 
 DECLARE @MyCursor CURSOR;
 DECLARE @MyCursor2 CURSOR;
@@ -63,3 +56,21 @@ BEGIN
     CLOSE @MyCursor ;
     DEALLOCATE @MyCursor;
 END;
+
+
+----Vérification après lancement du curseur : 
+--Assurance avec correspondance
+select cig.*, insu1.Name, insu1.providerid,  insu2.ProviderId  from CrossInsuranceGrid cig
+inner join Insurance insu1 on insu1.InsuranceId = cig.InsuranceId 
+inner join Insurance insu2 on insu2.InsuranceId = cig.DestinationInsuranceId
+order by insu1.ProviderId , insu1.Name,  insu2.ProviderId 
+
+--Assurance sans correspondance
+select insu.* from Insurance insu
+left join CrossInsuranceGrid cig on cig.InsuranceId = insu.InsuranceId 
+where cig.CrossInsuranceGridId is null
+
+--Assurance Avec Rachat partiel de franchise (Jeune Conducteur)	22	=>Assurance Avec Rachat partiel de franchise (0.56)
+--Assurance Avec Rachat partiel de franchise (Pro)	22 => Assurance Avec Rachat partiel de franchise (0.28)
+--Malus Sinistre	18 => Malus Sinistre (0.28)
+--Assurance Pro avec rachat partiel de franchise	19 => Assurance Avec Rachat partiel de franchise (0.28)
